418a5ad0370ed9afc24bb2de4f417f7f7019148d
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-07-07 20:08:39 -0300

    Update PlayerMovement.cs

--- OutPlayed/Assets/Scripts/PlayerMovement.cs
+++ OutPlayed/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator anim;
+    public SceneFader fader;
 
     float horizontalMove = 0f;
     public float runSpeed = 40f;
     bool jump = false;
+    public float restartDelay = 1f;
+    public Vector3 respawnPoint;
+
+    void Start()
+    {
+        respawnPoint = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
@@ -33,4 +41,18 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Floorlimit")
+        {
+            transform.position = respawnPoint;
+        }
+        else if (coll.tag == "CheckPoint")
+        {
+            respawnPoint = transform.position;
+        }
+
+    }
+
 }
