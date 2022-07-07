3d4c6be2879456aada3a5a50e02dc906bf8c0ef0
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-07-06 21:18:58 -0300

    Update MainMenu.cs

--- OutPlayed/Assets/MainMenu.cs
+++ OutPlayed/Assets/MainMenu.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public SceneFader fader;
+
     public void PlayGame() 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        fader.FadeTo();
     }
 
     public void QuitGame() 
