75b55a0dbe787b5bb15be56d963af6b94fb7cb5d
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-07-19 17:17:52 -0300

    Update EndGameCollider.cs

--- OutPlayed/Assets/Scripts/EndGameCollider.cs
+++ OutPlayed/Assets/Scripts/EndGameCollider.cs
@@ -6,12 +6,15 @@ public class EndGameCollider : MonoBehaviour
 {
     public GameObject endgameUI;
     public GameObject player;
+    public AudioSource audioS;
     
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            audioS.Stop();
+            GetComponent<AudioSource>().Play();
             endgameUI.SetActive(true);
             player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
 
