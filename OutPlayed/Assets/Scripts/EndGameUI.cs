0fb0319bedeb63bcb5ac8d0af64d3d082f7961ea
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-07-26 19:06:13 -0300

    Timer on EndgameUI

--- OutPlayed/Assets/Scripts/EndGameUI.cs
+++ OutPlayed/Assets/Scripts/EndGameUI.cs
@@ -1,11 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGameUI : MonoBehaviour
 {
     public SceneFader fader;
-  
+    public Text txtTimer2;
+    private bool timerfinish = false;
+    private float time;
+    
+    public void Start()
+    {
+        txtTimer2.text = "00:00";
+        time = 0;
+    }
+
+
+    void Update()
+    {
+        if (!timerfinish)
+        {
+            TimeCalculate();
+        }
+    }
+
+    public void TimeCalculate()
+    {
+        time += Time.deltaTime;
+        int min = (int)time / 60;
+        int seg = (int)time % 60;
+        txtTimer2.text = min.ToString() + ":" + seg.ToString().PadLeft(2, '0');
+    }
+
     public void PlayAgain() 
     {
         fader.FadeAgain();
