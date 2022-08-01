e86b94a8266c95461f34d01efff7199479d92d0e
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-08-01 19:28:12 -0300

    Add a single trigger time on checkpoints.

--- OutPlayed/Assets/Scripts/CheckPoint.cs
+++ OutPlayed/Assets/Scripts/CheckPoint.cs
@@ -6,15 +6,24 @@ public class CheckPoint : MonoBehaviour
 {
     public GameObject confettiEffect;
     public Transform checkpoint;
+    public bool isTrigger = false;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && isTrigger == false)
         {
             GameObject go = Instantiate(confettiEffect, checkpoint.position, checkpoint.rotation);
             Destroy(go, 2.5f);
             GetComponent<AudioSource>().Play();
             GetComponent<Animator>().enabled = true;
+            GetComponent<Collider2D>().enabled = false;
+            isTrigger = true;
         }
-    }  
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTrigger = false;
+    }
 }
