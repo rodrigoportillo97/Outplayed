deee36d229851427b232e5c8c0c5ca11d70bc212
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-08-12 19:59:56 -0300

    Add arrow Hit animation

--- OutPlayed/Assets/Scripts/Arrows.cs
+++ OutPlayed/Assets/Scripts/Arrows.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class Arrows : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float resetTime;
+    public Animator anim;
+
+    private void Start()
+    {
+       anim = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
         float movementSpeed = speed * Time.deltaTime;
         transform.Translate(movementSpeed, 0, 0);
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        speed = 0;
+        anim.enabled = true;
+        Destroy();
+    }
+
+    public void Destroy()
+    {
+        StartCoroutine(DestroyCR());
+    }
+
+    IEnumerator DestroyCR() 
+    {
+        yield return new WaitForSeconds(0.5f);
         Destroy(gameObject);
     }
 }
