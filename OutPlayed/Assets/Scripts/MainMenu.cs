b5e5eb4ec3d21e1be964addcf135bc46d37f0765
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-11-01 22:38:54 -0300

    Add New Game and Continue Game functionality to Main Menu

--- OutPlayed/Assets/Scripts/MainMenu.cs
+++ OutPlayed/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public SceneFader fader;
+    public Button continueGameButton;
 
     public void PlayGame() 
+    {
+        PlayerPrefs.DeleteAll();
+        fader.FadeTo();
+    }
+
+    public void Update()
+    {
+        KeyCheck();
+    }
+
+    public void KeyCheck() 
+    {
+        if (PlayerPrefs.HasKey($"Player_x"))
+        {
+            continueGameButton.IsInteractable();
+        }
+        else
+        {
+            
+        }
+    }
+
+    public void ContinueGameWhenActive() 
     {
         fader.FadeTo();
     }
@@ -15,7 +40,6 @@ public class MainMenu : MonoBehaviour
     public void QuitGame() 
     {
         Application.Quit();
-
     }
 
 
