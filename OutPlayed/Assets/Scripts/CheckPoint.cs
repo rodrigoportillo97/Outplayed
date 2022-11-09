6684563e8afcb4e4acb5e8815630f40941844c16
rodrigoportillo97 <rodrigo97.portillo@gmail.com>
2022-11-09 18:42:29 -0300

    Add SavedGameText popup when reaching a Checkpoint

--- OutPlayed/Assets/Scripts/CheckPoint.cs
+++ OutPlayed/Assets/Scripts/CheckPoint.cs
@@ -8,6 +8,7 @@ public class CheckPoint : MonoBehaviour
     public GameObject confettiEffect;
     public Transform checkpoint;
     public TMP_Text text;
+    public GameObject savedGameAnim;
 
     private void Start()
     {
@@ -17,11 +18,19 @@ public class CheckPoint : MonoBehaviour
     public void ActivateCheckPoint() 
     {
         GameObject go = Instantiate(confettiEffect, checkpoint.position, checkpoint.rotation);
+        GameSavedText();
         Destroy(go, 2.5f);
         GetComponent<AudioSource>().Play();
         GetComponent<Animator>().enabled = true;
         GetComponent<Collider2D>().enabled = false;
         text.enabled = true;
+    }
+
+    public void GameSavedText() 
+    {
+        GameObject savedGame = Instantiate(savedGameAnim);
+        savedGame.transform.SetParent(FindObjectOfType<IngameUI>().transform);
+        Destroy(savedGame, 10f);
     }
 
 
